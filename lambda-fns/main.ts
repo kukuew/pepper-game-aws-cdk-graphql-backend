9d0cb6ca734e344aaab4a73e84c9c3924d0af8f1
Mykola Smuzhanytsia <smuzhanytsia@gmail.com>
2023-03-04 15:19:11 +0100

    feat: add lambda, resolvers, dynamodb table

---
+++ lambda-fns/main.ts
@@ -0,0 +1,9 @@
+type AppSyncEvent = {
+  info: {
+    fieldName: string
+  }
+}
+
+exports.handler = async (event: AppSyncEvent) => {
+  return null
+}
--- lib/pepper-game-aws-cdk-graphql-backend-stack.ts
+++ lib/pepper-game-aws-cdk-graphql-backend-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from '@aws-cdk/core'
 import * as appsync from '@aws-cdk/aws-appsync'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as ddb from '@aws-cdk/aws-dynamodb'
 
 export class PepperGameAwsCdkGraphqlBackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -26,5 +28,50 @@ export class PepperGameAwsCdkGraphqlBackendStack extends cdk.Stack {
     new cdk.CfnOutput(this, 'PepperGame:GraphQL-API-Key', {
       value: api.apiKey || ''
     })
+
+    const starshipsLambda = new lambda.Function(this, 'AppSyncPepperGameStarshipsHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'main.handler',
+      code: lambda.Code.fromAsset('lambda-fns'),
+      memorySize: 1024
+    })
+
+    const starshipsLambdaDS = api.addLambdaDataSource('starshipsLambdaDataSource', starshipsLambda)
+
+    starshipsLambdaDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'getStarshipById'
+    })
+
+    starshipsLambdaDS.createResolver({
+      typeName: 'Query',
+      fieldName: 'listStarships'
+    })
+
+    starshipsLambdaDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'createStarship'
+    })
+
+    starshipsLambdaDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'updateStarship'
+    })
+
+    starshipsLambdaDS.createResolver({
+      typeName: 'Mutation',
+      fieldName: 'deleteStarship'
+    })
+
+    const table = new ddb.Table(this, 'StarshipsTable', {
+      billingMode: ddb.BillingMode.PAY_PER_REQUEST,
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING
+      }
+    })
+
+    table.grantFullAccess(starshipsLambda)
+    starshipsLambda.addEnvironment('STARSHIPS_TABLE', table.tableName)
   }
 }
