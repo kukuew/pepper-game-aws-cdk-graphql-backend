0a5d75aa8850feb61a06db3c42ec7078bd5b6350
Mykola Smuzhanytsia <smuzhanytsia@gmail.com>
2023-03-04 14:20:40 +0100

    feat: add appsync graphql api

--- lib/pepper-game-aws-cdk-graphql-backend-stack.ts
+++ lib/pepper-game-aws-cdk-graphql-backend-stack.ts
@@ -1,17 +1,30 @@
-import * as cdk from 'aws-cdk-lib'
-import { Construct } from 'constructs'
-
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core'
+import * as appsync from '@aws-cdk/aws-appsync'
 
 export class PepperGameAwsCdkGraphqlBackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    // The code that defines your stack goes here
+    const api = new appsync.GraphqlApi(this, 'PepperGame:API', {
+      name: 'cdk-pepper-game-appsync-api',
+      schema: appsync.Schema.fromAsset('graphql/schema.graphql'),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(30))
+          }
+        }
+      },
+      xrayEnabled: true
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'PepperGameAwsCdkGraphqlBackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.CfnOutput(this, 'PepperGame:GraphQL-API-URL', {
+      value: api.graphqlUrl
+    })
+
+    new cdk.CfnOutput(this, 'PepperGame:GraphQL-API-Key', {
+      value: api.apiKey || ''
+    })
   }
 }
